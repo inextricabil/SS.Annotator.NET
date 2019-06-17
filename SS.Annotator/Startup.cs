68f32e2d53b25daef5e2d977a4b7289a5aad5eb5
Serban Boghiu <pincellodev14@outlook.com>
2019-06-17 03:46:54 +0300

    Add project files.

---
+++ SS.Annotator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SS.Annotator.Startup))]
+namespace SS.Annotator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
